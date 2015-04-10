7cd286a05f03556caa873ec15a955d5ca167e043
Luke Maser <lukemaser@gmail.com>
2015-04-10 17:21:12 -0400

    Finished jQuery, added instructions

--- js/app.js
+++ js/app.js
@@ -4,20 +4,41 @@ $(document).ready(function() {
 		$('.ryu-ready').show();
 	})
 	.mouseleave(function() {
-		$('.ryu-ready').hide();
 		$('.ryu-still').show();
+		$('.ryu-ready').hide();
 	})
 	.mousedown(function() {
-		console.log('mousedown');
-		//play hadouken sound
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
-		//show hadouken and animate it to the right of the screen
+		$('.hadouken').finish().show().animate(
+  			{'left': '1020px'},
+  			500,
+  			function(){
+    			$(this).hide();
+    			$(this).css('left', '520px');
+  			});
 	})
 	.mouseup(function() {
-		console.log('mouseup');
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
-	});
-});
+	})
+	$(document).keydown(function(e) {
+		if (e.keyCode == 88) {
+			$('.ryu-cool').show();
+			$('.ryu-ready').hide();
+		}
+	})
+	.keyup(function(e) {
+		if (e.keyCode == 88) {
+			$('.ryu-cool').hide();
+			$('.ryu-ready').show();
+		}
+	});	
+  });
+function playHadouken() {
+	$('#hadouken-sound') [0].volume = 0.5;
+	$('#hadouken-sound') [0].load();
+	$('#hadouken-sound') [0].play();
+}
+
